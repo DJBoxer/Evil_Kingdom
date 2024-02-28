c13a9d8a496abc3ccfb6399a2f6f2045f8473f28
DJBoxer <djboxer2010@gmail.com>
2024-02-27 17:49:59 -0800

    Added explanations to code

--- Evil_kingdom/src/main/Main.java
+++ Evil_kingdom/src/main/Main.java
@@ -6,13 +6,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		JFrame window = new JFrame();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setResizable(false);
-		window.setTitle("Evil Kingdom");
+		JFrame window = new JFrame();                           // Create JFrame
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // User closes window with (X)
+		window.setResizable(false);                             // Do not allow to resize window
+		window.setTitle("Evil Kingdom");                        // Set title for window
 		
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);
+		window.setLocationRelativeTo(null);                     // Window will appear in center 
+		window.setVisible(true);                                // Makes window appear to view
 		
 	}
 
