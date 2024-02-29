a8a8bdb4893f06b1c5e0fd662734f658c67a782b
DJBoxer <djboxer2010@gmail.com>
2024-02-28 17:11:26 -0800

    Add Screen Setttings

--- Evil_kingdom/src/main/GamePanel.java
+++ Evil_kingdom/src/main/GamePanel.java
@@ -1,5 +1,12 @@
 package main;
 
-public class GamePanel {
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+	
+	//SCREEN SETTINGS
+	final int originalTileSize = 16;                 // 16x16 Tile
+	final int scale = 3;                             // 16x3 = 48
+	final int tileSize = originalTileSize * scale;   // 48x48 Tile
 
 }
